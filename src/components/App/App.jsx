6368f1f1eb6cd8ddc24168ad3d7d3271e314cf71
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-02-10 20:48:24 -0700

    Add hello world component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Counter from '../Counter/Counter'
+import Counter from '../Counter/Counter';
+import HelloWorld from '../HelloWorld/HelloWorld';
 
 export default class App extends Component{
   render() {
     return (
       <div>
+        <HelloWorld/>
         <Counter/>
       </div>
     )
