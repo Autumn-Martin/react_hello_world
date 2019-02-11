7b0f871192f8cc10b8dd7a6e104dd655fffb724d
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-02-10 20:45:23 -0700

    Add app component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Counter from '../Counter/Counter'
+
+export default class App extends Component{
+  render() {
+    return (
+      <div>
+        <Counter/>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './counter';
+import App from './components/App/App';
 
 // const element = <h1>Hello World</h1>;
 //
 // ReactDOM.render(element, document.getElementById('root'));
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // For JSX, use self closing notation if nothing is between the opening & closing tags
